ef59e07d84efdb5ecaf412160a674abe711245df
Allachemist <131195743+Allachemist@users.noreply.github.com>
2023-06-07 17:17:59 +0300

    added chanel brush

---
+++ src/ChanelBrush.java
@@ -0,0 +1,19 @@
+public class ChanelBrush  {
+    int size;
+    public static final String BRAND = "Chanel";
+    String type;
+    double price;
+
+    public ChanelBrush() {
+    }
+
+
+        public ChanelBrush(int size, String type, double price) {
+        this.size = size;
+        this.type = type;
+        this.price = price;
+    }
+    public ChanelBrush(int number) {
+
+    }
+}
